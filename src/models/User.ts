9cfc897270a1057fe8a02de8bcdd526df56c134e
StanleeO <stanlee.okwii@gmail.com>
2024-11-27 22:15:01 +0000

    hash user password on pre save

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,6 @@
 import { Model, model, Schema } from "mongoose";
+import { genSalt, hash } from "bcryptjs";
+
 import { IUser, IUserDocument } from "../interfaces/User";
 
 type UserModel = Model<IUser, {}, {}>
@@ -8,14 +10,20 @@ export const UserSchema: Schema<IUserDocument, UserModel> = new Schema<IUserDocu
     type: String,
     unique: true,
     required: true,
+    min: 5,
+    max: 100
   },
   firstName: {
     type: String,
     required: true,
+    min: 5,
+    max: 100
   },
   lastName: {
     type: String,
     required: true,
+    min: 5,
+    max: 100
   },
   password: {
     type: String,
@@ -24,8 +32,10 @@ export const UserSchema: Schema<IUserDocument, UserModel> = new Schema<IUserDocu
 },  { timestamps: true });
 
 // Hash passport on pre save
-UserSchema.pre('save', function(): void {
-    console.log(this.firstName); // TypeScript knows that `this` is a `mongoose.Document & User` by default
+UserSchema.pre('save', async function() {
+  const salt = await genSalt(5);
+  const hashedPassword = await hash(this.password, salt);
+  this.password = hashedPassword;
 });
 
 export const User = model<IUserDocument>("User", UserSchema);
