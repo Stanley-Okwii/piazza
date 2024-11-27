2ce6787e4aae00caebb9bbe38f27dedeaf4e0672
StanleeO <stanlee.okwii@gmail.com>
2024-11-27 22:49:46 +0000

    add types to verifyAuth util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,35 @@
-import { Response } from "express";
+import { Response, Request, NextFunction } from "express";
+import { verify, JwtPayload } from "jsonwebtoken";
+import { env } from 'node:process';
+
+import { UserJwtPayload } from "../interfaces"
 
 
 export const validateRequest = (
   validate: Function,
   requestBody: Object,
   response: Response
-) => {
+): void => {
   const { error } = validate(requestBody);
   if (error) {
     response.status(400).json({ message: error["details"][0]["message"] });
     return;
   }
 };
+
+
+export const verifyAuth = (request: Request, response: any, next: NextFunction): void => {
+    const token: string | undefined = request.header('Auth-Token')
+    if(!token){
+        return response.status(401).send({message:'No token provided'})
+    }
+    try{
+        const payload: JwtPayload | String  = verify(token, `${env.TOKEN_SECRET}`);
+        const userId: string = (payload as UserJwtPayload)._id;
+        response.set('userId', userId); // Set userId to response headers which is later used to identify and logged in user
+        next();
+    }catch(error){
+        return response.status(401).send({message:'Invalid token'})
+    }
+}
+
